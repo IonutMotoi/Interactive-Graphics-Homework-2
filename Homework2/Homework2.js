729bb5c1bf36c66b5efa191b7a961d26fb92789e
Ionut Motoi <32934655+IonutMotoi@users.noreply.github.com>
2020-06-11 14:29:09 +0200

    Added textures and tail

--- Homework2/Homework2.js
+++ Homework2/Homework2.js
@@ -22,6 +22,13 @@ var vertices = [
     vec4( 0.5, -0.5, -0.5, 1.0 )
 ];
 
+var texCoord = [
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
+];
+
 // IDs
 var torsoId = 0;
 var headId  = 1;
@@ -33,9 +40,10 @@ var leftUpperLegId = 6;
 var leftLowerLegId = 7;
 var rightUpperLegId = 8;
 var rightLowerLegId = 9;
+var tailID = 10
 
-var lowerTreeId = 10;
-var upperTreeId = 11;
+var lowerTreeId = 11;
+var upperTreeId = 12;
 
 // Bear dimensions
 var torsoHeight = 5.0;
@@ -50,6 +58,8 @@ var lowerLegHeight = 2.0;
 var upperLegHeight = 3.0;
 var headHeight = 2.0;
 var headWidth = 2.0;
+var tailHeight = 0.8;
+var tailWidth = 0.8;
 
 var theta = [0, 0, 110, -50, 70, -20, 115, 10, 75, 30];
 
@@ -61,16 +71,61 @@ var upperTreeWidth = 12.0;
 
 var stack = [];
 var figure = [];
-var numNodes = 12;
+var numNodes = 13;
 
 for( var i=0; i<numNodes; i++) figure[i] = createNode(null, null, null, null);
 
 var vBuffer;
+var tBuffer;
 var modelViewLoc;
 
 var pointsArray = [];
+var texCoordsArray = [];
 
-//-------------------------------------------
+// Texture variables
+var texTrunk, texLeaves;
+var texBody, texHead;
+
+var posBearX = -15.0;
+var posTreeX = 10.0;
+
+var key = 1;
+
+//  Texture configuration
+function configureTexture(imgBody, imgHead, imgTrunk, imgLeaves) {
+    texBody = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texBody);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, imgBody);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    texHead = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texHead);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, imgHead);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    texTrunk = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texTrunk);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, imgTrunk);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    texLeaves = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texLeaves);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, imgLeaves);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
+
 
 function scale4(a, b, c) {
    var result = mat4();
@@ -79,8 +134,6 @@ function scale4(a, b, c) {
    result[10] = c;
    return result;
 }
-
-//--------------------------------------------
 
 
 function createNode(transform, render, sibling, child){
@@ -101,20 +154,18 @@ function initNodes(Id) {
     switch(Id) {
 
     case torsoId:
-    m = translate(0.0, -10.0, 0.0);
+    m = translate(posBearX, -10.0, 0.0);
     m = mult(m, rotate(90, vec3(0, 0, 1)) );
     m = mult(m, rotate(90, vec3(0, 1, 0)) );
     m = mult(m, rotate(theta[torsoId], vec3(0, 0, 1)))
-    figure[torsoId] = createNode( m, torso, lowerTreeId, headId );
+    figure[torsoId] = createNode(m, torso, lowerTreeId, headId );
     break;
 
     case headId:
     m = translate(0.0, torsoHeight, headWidth/4.0);
-	m = mult(m, rotate(theta[headId], vec3(1, 0, 0)))
-    m = mult(m, translate(0.0, 0.0, 0.0));
-    figure[headId] = createNode( m, head, leftUpperArmId, null);
+	m = mult(m, rotate(90, vec3(0, 1, 0)))
+    figure[headId] = createNode(m, head, leftUpperArmId, null);
     break;
-
 
     case leftUpperArmId:
     m = translate(-0.5*torsoWidth, 0.9*torsoHeight, 0.0);
@@ -125,47 +176,52 @@ function initNodes(Id) {
     case rightUpperArmId:
     m = translate(0.5*torsoWidth, 0.9*torsoHeight, 0.0);
 	m = mult(m, rotate(theta[rightUpperArmId], vec3(1, 0, 0)));
-    figure[rightUpperArmId] = createNode( m, rightUpperArm, leftUpperLegId, rightLowerArmId );
+    figure[rightUpperArmId] = createNode(m, rightUpperArm, leftUpperLegId, rightLowerArmId );
     break;
 
     case leftUpperLegId:
     m = translate(-0.5*torsoWidth, 0.1*torsoHeight, 0.0);
 	m = mult(m , rotate(theta[leftUpperLegId], vec3(1, 0, 0)));
-    figure[leftUpperLegId] = createNode( m, leftUpperLeg, rightUpperLegId, leftLowerLegId );
+    figure[leftUpperLegId] = createNode(m, leftUpperLeg, rightUpperLegId, leftLowerLegId );
     break;
 
     case rightUpperLegId:
     m = translate(0.5*torsoWidth, 0.1*torsoHeight, 0.0);
 	m = mult(m, rotate(theta[rightUpperLegId], vec3(1, 0, 0)));
-    figure[rightUpperLegId] = createNode( m, rightUpperLeg, null, rightLowerLegId );
+    figure[rightUpperLegId] = createNode(m, rightUpperLeg, tailID, rightLowerLegId );
     break;
 
     case leftLowerArmId:
-    m = translate(0.0, upperArmHeight, 0.0);
+    m = translate(0.0, 0.9*upperArmHeight, 0.0);
     m = mult(m, rotate(theta[leftLowerArmId], vec3(1, 0, 0)));
-    figure[leftLowerArmId] = createNode( m, leftLowerArm, null, null );
+    figure[leftLowerArmId] = createNode(m, leftLowerArm, null, null );
     break;
 
     case rightLowerArmId:
-    m = translate(0.0, upperArmHeight, 0.0);
+    m = translate(0.0, 0.9*upperArmHeight, 0.0);
     m = mult(m, rotate(theta[rightLowerArmId], vec3(1, 0, 0)));
-    figure[rightLowerArmId] = createNode( m, rightLowerArm, null, null );
+    figure[rightLowerArmId] = createNode(m, rightLowerArm, null, null );
     break;
 
     case leftLowerLegId:
-    m = translate(0.0, upperLegHeight, 0.0);
+    m = translate(0.0, 0.9*upperLegHeight, 0.0);
     m = mult(m, rotate(theta[leftLowerLegId], vec3(1, 0, 0)));
-    figure[leftLowerLegId] = createNode( m, leftLowerLeg, null, null );
+    figure[leftLowerLegId] = createNode(m, leftLowerLeg, null, null );
     break;
 
     case rightLowerLegId:
-    m = translate(0.0, upperLegHeight, 0.0);
+    m = translate(0.0, 0.9*upperLegHeight, 0.0);
     m = mult(m, rotate(theta[rightLowerLegId], vec3(1, 0, 0)));
-    figure[rightLowerLegId] = createNode( m, rightLowerLeg, null, null );
+    figure[rightLowerLegId] = createNode(m, rightLowerLeg, null, null );
     break;
-    
+
+    case tailID:
+    m = translate(0.0, 0.0, 0.0);
+    figure[tailID] = createNode(m, tail, null, null);
+    break;
+
     case lowerTreeId:
-    m = translate(10.0, -15.0, 0.0);
+    m = translate(posTreeX, -15.0, 0.0);
     figure[lowerTreeId] = createNode(m, lowerTree, null, upperTreeId );
     break;
 
@@ -174,8 +230,8 @@ function initNodes(Id) {
     figure[upperTreeId] = createNode(m, upperTree, null, null);
     break;
     }
-
 }
+
 
 function traverse(Id) {
     if(Id == null) return;
@@ -187,7 +243,11 @@ function traverse(Id) {
     if(figure[Id].sibling != null) traverse(figure[Id].sibling);
 }
 
+
 function torso() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texBody);
+    gl.uniform1i(gl.getUniformLocation( program, "uTex"), 0);
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5*torsoHeight, 0.0) );
     instanceMatrix = mult(instanceMatrix, scale( torsoWidth, torsoHeight, torsoWidth));
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
@@ -198,7 +258,18 @@ function head() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0 ));
 	instanceMatrix = mult(instanceMatrix, scale(headWidth, headHeight, headWidth) );
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
-    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+    for(var i=0; i<3; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+
+    // Draw face
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texHead);
+    gl.uniform1i(gl.getUniformLocation( program, "uTex"), 0);
+    gl.drawArrays(gl.TRIANGLE_FAN, 4*3, 4);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texBody);
+    gl.uniform1i(gl.getUniformLocation( program, "uTex"), 0);
+    for(var i=4; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
 }
 
 function leftUpperArm() {
@@ -252,12 +323,22 @@ function rightUpperLeg() {
 
 function rightLowerLeg() {
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * lowerLegHeight, 0.0) );
-	instanceMatrix = mult(instanceMatrix, scale(lowerLegWidth, lowerLegHeight, lowerLegWidth) )
+	instanceMatrix = mult(instanceMatrix, scale(lowerLegWidth, lowerLegHeight, lowerLegWidth) );
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
+    for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
+}
+
+function tail() {
+    instanceMatrix = mult(modelViewMatrix, translate(0.0, -0.5 * tailHeight, 0.0) );
+	instanceMatrix = mult(instanceMatrix, scale(tailWidth, tailHeight, tailWidth) );
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
     for(var i =0; i<6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4*i, 4);
 }
 
 function lowerTree() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texTrunk);
+    gl.uniform1i(gl.getUniformLocation( program, "uTex"), 0);
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * lowerTreeHeight, 0.0) );
 	instanceMatrix = mult(instanceMatrix, scale(lowerTreeWidth, lowerTreeHeight, lowerTreeWidth) );
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
@@ -265,6 +346,9 @@ function lowerTree() {
 }
 
 function upperTree() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texLeaves);
+    gl.uniform1i(gl.getUniformLocation( program, "uTex"), 0);
     instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * upperTreeHeight, 0.0) );
 	instanceMatrix = mult(instanceMatrix, scale(upperTreeWidth, upperTreeHeight, upperTreeWidth) )
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix) );
@@ -276,6 +360,11 @@ function quad(a, b, c, d) {
      pointsArray.push(vertices[b]);
      pointsArray.push(vertices[c]);
      pointsArray.push(vertices[d]);
+
+     texCoordsArray.push(texCoord[0]);
+     texCoordsArray.push(texCoord[1]);
+     texCoordsArray.push(texCoord[2]);
+     texCoordsArray.push(texCoord[3]);
 }
 
 
@@ -299,6 +388,8 @@ window.onload = function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
+    gl.enable(gl.DEPTH_TEST);
+
     //
     //  Load shaders and initialize attribute buffers
     //
@@ -310,7 +401,8 @@ window.onload = function init() {
 
     projectionMatrix = ortho(-20.0, 20.0, -20.0, 20.0, -20.0, 20.0);
     
-    modelViewMatrix = mat4();
+    //modelViewMatrix = mat4();
+    modelViewMatrix = rotate(30, vec3(0,1,0));
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix")
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
@@ -319,13 +411,27 @@ window.onload = function init() {
     cube();
 
     vBuffer = gl.createBuffer();
-
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
     var positionLoc = gl.getAttribLocation( program, "aPosition" );
     gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
+
+    tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+
+    var texCoordLoc = gl.getAttribLocation(program, "aTexCoord");
+    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(texCoordLoc);
+
+    var imgBody = document.getElementById("imgBody");
+    var imgHead = document.getElementById("imgHead")
+    var imgTrunk = document.getElementById("imgTrunk");
+    var imgLeaves = document.getElementById("imgLeaves");
+
+    configureTexture(imgBody, imgHead, imgTrunk, imgLeaves);
 
     document.getElementById("slider0").onchange = function(event) {
         theta[torsoId ] = event.target.value;
@@ -375,7 +481,7 @@ window.onload = function init() {
 
 
 var render = function() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
     traverse(torsoId);
     requestAnimationFrame(render);
 }
